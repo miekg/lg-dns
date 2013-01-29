bb276f446c1c7940903c4c56f88c3fb754577bd6
Miek Gieben <miek@miek.nl>
2013-01-29 14:10:32 +0100

    Remove the foo I had

--- formats.go
+++ formats.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"github.com/miekg/unbound"
 	"net/http"
 )
 
 func Html(w http.ResponseWriter, u *unbound.Result) {
+	for _, r := range u.Rr {
+		fmt.Fprintf(w, "%s\n", r.String())
+	}
 }
 
 func Json(w http.ResponseWriter, u *unbound.Result) {
+	for _, r := range u.Rr {
+		fmt.Fprintf(w, "%s\n", r.String())
+	}
 }
 
 func Zone(w http.ResponseWriter, u *unbound.Result) {
+	for _, r := range u.Rr {
+		fmt.Fprintf(w, "%s\n", r.String())
+	}
 }
 
 func Text(w http.ResponseWriter, u *unbound.Result) {
+	for _, r := range u.Rr {
+		fmt.Fprintf(w, "%s\n", r.String())
+	}
 }
 
 func Xml(w http.ResponseWriter, u *unbound.Result) {
+	for _, r := range u.Rr {
+		fmt.Fprintf(w, "%s\n", r.String())
+	}
 }
